b8a5eef2e757ed68928b3ea2cc5586e15b988b1f
Medizonal <121202731+Medizonal@users.noreply.github.com>
2025-06-23 01:29:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
-def add(a, b):
+from PySide6.QtWidgets import QApplication, QLabel, QMainWindow
+from PySide6.QtCore import Qt
+import sys
+
+def add(a, b) -> float:
     return a + b
 
-def subtract(a, b):
+def subtract(a, b) -> float:
     return a - b
 
-def divide(a, b):
+def divide(a, b) -> float:
     return a / b
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Simple PySide6 App")
+
+        label = QLabel("Hi", alignment=Qt.AlignCenter)
+        self.setCentralWidget(label)
+
 
 if __name__ == "__main__":
     print(add(10, 5))
     print(divide(20, 4))
+
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.resize(300, 200)
+    window.show()
+    sys.exit(app.exec())
